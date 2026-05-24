6a6f2f4115fa000a1db45485ab4fb2a204d7ca2f
Jomar Milan <jomarm@jomarm.com>
2026-05-23 20:25:22 -0700

    Use markview module

--- config/plug/ui/markview.nix
+++ config/plug/ui/markview.nix
@@ -1,26 +1,13 @@
-{ pkgs, ... }:
+{ ... }:
 {
-	extraPlugins = with pkgs.vimUtils; [
-		(buildVimPlugin {
-			pname = "markview.nvim";
-			version = "unstable";
-			src = pkgs.fetchFromGitHub {
-				owner = "OXY2DEV";
-				repo = "markview.nvim";
-				rev = "main";
-				hash = "sha256-aArWqxqbBHDyTaMY6hXwxXoFf8l/j7yOJYKunxO7kis=";
-			};
-		})
-	];
-
-	extraConfigLua = ''
-		local presets = require("markview.presets");
-		require('markview').setup({
-				markdown = {
-					 headings = presets.headings.glow,
-					 horizontal_rules = presets.horizontal_rules.dashed,
-					 tables = presets.single
-				}
-			});
-		'';
+  plugins.markview = {
+    enable = true;
+    settings = {
+      markdown = {
+        headings.__raw = "require(\"markview.presets\").headings.glow";
+        horizontal_rules.__raw = "require(\"markview.presets\").horizontal_rules.dashed";
+        tables.__raw = "require(\"markview.presets\").single";
+      };
+    };
+  };
 }
